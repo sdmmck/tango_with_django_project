6c6f7e6d913589e1070b2f22e5628094cb7d1ff3
sdmmck <sarahdimmock93@gmail.com>
2019-01-22 21:29:37 +0000

    completed 5.7

--- populate_rango.py
+++ populate_rango.py
@@ -1,9 +1,12 @@
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
 
 import django
+
 django.setup()
 from rango.models import Category, Page
+
 
 def populate():
     # Create lists of dictionaries and then create a dictionary of dictionaries
@@ -32,25 +35,27 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat)
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], views=0)
 
     for c in Category.objects.all():
-        for p in Page.objects.filter(category = c):
-            print("- {0} - {1}".format(str(c), str(p))
+        for p in Page.objects.filter(category=c):
+            print("- {0} - {1}".format(str(c), str(p)))
+
 
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url=url
-    p.views=views
+    p.url = url
+    p.views = views
     p.save()
     return p
+
 
 def add_cat(name):
     c = Category.objects.get_or_create(name=name)[0]
     c.save()
     return c
 
+
 if __name__ == '__main__':
     print("Starting Rango population script...")
     populate()
-
