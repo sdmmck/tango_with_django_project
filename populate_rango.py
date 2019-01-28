5500b254df6da6b4993e77a43f3a44b5a3a5572d
sdmmck <sarahdimmock93@gmail.com>
2019-01-28 23:33:07 +0000

    Ch 6 and 7 (excluding ch7 exercises at end)

--- populate_rango.py
+++ populate_rango.py
@@ -12,20 +12,21 @@ def populate():
     # Create lists of dictionaries and then create a dictionary of dictionaries
 
     python_pages = [
-        {"title": "Official Python Tutorial", "url": "http://docs.python.org/2/tutorial/"},
-        {"title": "How to Think like a Computer Scientist", "url": "http://www.greenteapress.com/thinkpython/"},
-        {"title": "Learn Python in 10 Minutes", "url": "http://www.korokithakis.net/tutorials/python/"}
+        {"title": "Official Python Tutorial", "url": "http://docs.python.org/2/tutorial/", "views": 58},
+        {"title": "How to Think like a Computer Scientist", "url": "http://www.greenteapress.com/thinkpython/",
+            "views": 32},
+        {"title": "Learn Python in 10 Minutes", "url": "http://www.korokithakis.net/tutorials/python/", "views": 8}
     ]
 
     django_pages = [
-        {"title": "Official Django Tutorial", "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
-        {"title": "Django Rocks", "url": "http://www.djangorocks.com/"},
-        {"title": "How to Tango with Django", "url": "http://www.tangowithdjango.com/"},
+        {"title": "Official Django Tutorial", "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 485},
+        {"title": "Django Rocks", "url": "http://www.djangorocks.com/", "views": 328},
+        {"title": "How to Tango with Django", "url": "http://www.tangowithdjango.com/", "views": 17},
     ]
 
     other_pages = [
-        {"title": "Bottle", "url": "http://bottlepy.org/docs/dev/"},
-        {"title": "Flask", "url": "http://flask.pocoo.org"},
+        {"title": "Bottle", "url": "http://bottlepy.org/docs/dev/", "views": 87},
+        {"title": "Flask", "url": "http://flask.pocoo.org", "views": 293},
     ]
 
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
@@ -33,18 +34,19 @@ def populate():
             "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
 
     for cat, cat_data in cats.items():
-        views = cat_data['views']
-        likes = cat_data['likes']
+        views = cat_data["views"]
+        likes = cat_data["likes"]
         c = add_cat(cat, views, likes)
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"], views=0)
+            p_views = p["views"]
+            add_page(c, p["title"], p["url"], p_views)
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
 
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url = url
     p.views = views
--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,85 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary which will be passed
+    # to the template engine.
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    #{'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # Render response and send it back
     return render(request, 'rango/index.html', context=context_dict)
         # HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+
 
 def about(request):
     context_dict = {'boldmessage': "Sarah!"}
     return render(request, 'rango/about.html', context=context_dict)
         # HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    form = PageForm()
+    if request.method == 'POST':
+        if category:
+            page = form.save(commit=False)
+            page.category = category
+            page.views = 0
+            page.save()
+            return show_category(request, category_name_slug)
+        else:
+            print(form.errors)
+
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context_dict)
+# PAGE 91 OF TANGO WITH DJANGO
