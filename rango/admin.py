1c7777b033dbcc69a93553ce613fe45a87f2db8a
sdmmck <sarahdimmock93@gmail.com>
2019-02-07 01:34:00 +0000

    ch 9 without final exercises

--- rango/admin.py
+++ rango/admin.py
@@ -1,11 +1,11 @@
 from typing import Tuple
 
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
+    prepopulated_fields = {'slug': ('name',)}
 
 
 class PageAdmin(admin.ModelAdmin):
@@ -14,3 +14,4 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -110,6 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+LOGIN_URL = '/rango/login/'
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
